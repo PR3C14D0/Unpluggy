a1d95d622e60d5f9632dc9cd56979f2358fad754
Preciado <aarongarpre@gmail.com>
2024-04-10 00:44:24 +0200

    Started websocket connection to Discord gateway.

--- src/discord/client.rs
+++ src/discord/client.rs
@@ -1,11 +1,38 @@
+use std::error::Error;
+use serde::{Deserialize};
+use tokio::net::TcpStream;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message, WebSocketStream, MaybeTlsStream};
+
+#[derive()]
 pub struct Client {
-    token: String,
-    intents: u32
+    pub token: String,
+    pub intents: u32,
+    client: Option<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+}
+
+#[derive(Deserialize)]
+struct Gateway {
+    url: String
 }
 
 
 impl Client {
-    pub fn new(token: String, intents: u32) -> Client {
-        Client { token, intents }
+    pub fn new(token: String, intents: u32) -> Self {
+        Client { token, intents, client: None }
+    }
+
+    pub async fn login(&mut self) -> Result<(), Box<dyn Error + Send + Sync>> {
+        let res: Gateway = reqwest::get("https://discord.com/api/v10/gateway")
+            .await?
+            .json::<Gateway>()
+            .await?;
+
+        let (socket, _) = connect_async(res.url).await.expect("Failed to connect to websocket");
+        self.client = Some(socket);
+
+        println!("Handshake between client and websocket done");
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 use std::error::Error;
+use std::fs::{File};
+use serde::Deserialize;
+use std::path::Path;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Config {
+    token: String,
+    intents: u32
+}
 
 mod discord {
     pub mod client;
@@ -8,6 +18,11 @@ use discord::client::Client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let json_file_path: &Path = Path::new("./config.json");
+    let file: File = File::open(json_file_path)?;
+    let config: Config = serde_json::from_reader(file).expect("Error while reading JSON file.");
 
+    let mut discord_client: Client = Client::new(config.token, config.intents);
+    discord_client.login().await.expect("Error logging into discord.");
     Ok(())
 }
